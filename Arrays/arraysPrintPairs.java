805e09a05e43051d732ef0720bace33a92d7165e
Aniket Chauhan <aniketchauhan4163@gmail.com>
2026-03-23 23:49:56 +0530

    Added Function To Print Pairs from Arrays

---
+++ Arrays/arraysPrintPairs.java
@@ -0,0 +1,27 @@
+
+package Arrays;
+public class arraysPrintPairs {
+
+public static void printpairs(int numbers[]) {
+    int tp = 0;
+    for(int i=0;i < numbers.length; i++) {
+        for( int j = i+1;j<numbers.length; j++) {
+            System.out.print( "(" +numbers[i] + "," +  numbers[j] + ")" );
+              tp++;       }
+ System.out.println();
+    }
+    System.out.println("total no. of pairs = " + tp);    //  (n(n-1)) / 2  #Arth..Progg..
+}
+public static void main(String[] args) {
+    int numbers[] = {2,4,6,8,10};
+    printpairs(numbers);
+}
+
+}
+
+    
+
+
+
+
+
