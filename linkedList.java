dda180ee3531d290824cc453a9bc3203632f1fc7
Aniket Chauhan <aniketchauhan4163@gmail.com>
2026-04-05 10:11:57 +0530

    Added Remove and Add Node

--- linkedList.java
+++ linkedList.java
@@ -11,8 +11,11 @@ public class linkedList {
     }
         public static Node head;
         public static Node tail;
+               static int size;
+
         public void addFirst(int data){
             Node newNode = new Node(data);
+            size++;
             if (head == null) {
                 head = tail = newNode;
                 return;
@@ -24,6 +27,7 @@ public class linkedList {
 
         public void addLast(int data){
             Node newNode = new Node(data);
+            size++;
             if (head == null) {
                 head = tail = newNode;
                 return;
@@ -41,12 +45,75 @@ public class linkedList {
                 temp = temp.next;
             }System.out.println();
         }
+
+        public void add(int idx,int data){
+            if (idx ==0) {
+                addFirst(data);
+                return;
+            }
+            Node newNode = new Node(data);
+            size++;
+            Node temp = head;
+            int i=0;
+            while (i<idx-1) {
+                temp=temp.next;
+                i++;
+            }
+            newNode.next=temp.next;
+            temp.next = newNode;
+        }
+
+        public int removeFirst(){
+            if (size ==0) {
+                return Integer.MAX_VALUE;
+            }
+            else if(size == 1){
+                int n = head.data;
+                head = tail = null;
+                size=0;
+                return n;
+            }
+            int n = head.data;
+            head = head.next;
+            size--;
+            return n;
+        }
+
+        public int removeLast(){
+            if (size == 0) {
+                System.out.println("Ll is empty");
+                return Integer.MIN_VALUE;
+            }
+            else if (size == 1) {
+                int n = tail.data;
+                head = tail = null;
+                size = 0;
+                return n;
+            }
+            Node prev = head;
+            for (int i = 0; i < size-2; i++) {
+                prev = prev.next;
+            }
+            int n = tail.data; //or prev.next.data;
+            prev.next = null;
+            tail = prev;
+            size--;
+            return n;
+
+        }
+
     public static void main(String[] args) {
         linkedList ll = new linkedList();
         ll.addFirst(2);
         ll.addFirst(1);
         ll.addLast(3); 
         ll.addLast(4); 
+        ll.add(2,99);
         ll.print();
+        ll.removeFirst();
+        ll.print();
+        ll.removeLast();
+        ll.print();
+        System.out.println(size);
     }
 }
