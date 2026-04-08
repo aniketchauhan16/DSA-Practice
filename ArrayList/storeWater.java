ad48205243e814390fc6dd230a34578dcac9f416
Aniket Chauhan <aniketchauhan4163@gmail.com>
2026-04-08 07:40:22 +0530

    Add ArrayList/storeWater.java

---
+++ ArrayList/storeWater.java
@@ -0,0 +1,44 @@
+package ArrayList;
+import java.util.ArrayList;
+
+public class storeWater {
+
+    public static int StoreWater(ArrayList<Integer> height ){
+        int maxWater =0;
+        int lp=0;
+        int rp= height.size()-1;
+        while (lp<rp) {
+            int ht = Math.min(height.get(lp),height.get(rp) );
+            int wt = rp-lp;
+            int currWater = ht*wt;
+            maxWater = Math.max(maxWater, currWater);
+             
+
+                 if (height.get(lp) < height.get(rp)) {
+                    lp++;    
+                 } 
+                 else{                     //include both lp > & = rp; 
+                    rp--;
+                 }
+        }
+        return maxWater;
+
+    }
+    public static void main(String[] args) {
+        ArrayList<Integer> height = new ArrayList<>();
+
+        height.add(1);
+        height.add(8);
+        height.add(6);
+        height.add(2);
+        height.add(5);
+        height.add(4);
+        height.add(8);
+        height.add(3);
+        height.add(7);
+
+        int n = StoreWater(height);
+        System.out.println(" Max water is : " + n);
+    }
+    
+}
