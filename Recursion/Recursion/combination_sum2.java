486d0a745505427415f01f2a1ed4cf7a8117fa9c
Aniket Chauhan <aniketchauhan4163@gmail.com>
2026-04-08 07:41:03 +0530

    Add Recursion/Recursion/combination_sum2.java

---
+++ Recursion/Recursion/combination_sum2.java
@@ -0,0 +1,31 @@
+package Recursion.Recursion;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class combination_sum2 {
+    
+    private void findcombinations(int index,int[] arr,int target,List<List<Integer>> ans,List<Integer> ds ){
+        if (target==0) {
+            ans.add(new ArrayList<>(ds));
+            return;
+        }
+        for(int i=index;i<arr.length;i++){
+            if (i>index && arr[i] == arr[i-1]) continue;
+            if(arr[i] > target ) break;
+            
+            ds.add(arr[i]);
+            findcombinations(i+1, arr, target-arr[i], ans, ds);
+            ds.remove(ds.size()-1);
+        }
+    }
+
+ 
+      public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        List<List<Integer>> ans = new ArrayList<>();
+        Arrays.sort(candidates);
+        findcombinations(0,candidates,target,ans,new ArrayList<>());
+        return ans;
+    }}
+
