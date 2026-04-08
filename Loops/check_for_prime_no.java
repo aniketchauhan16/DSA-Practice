386a7902bbcf005f1b3f42297fb50b79a892aca5
Aniket Chauhan <aniketchauhan4163@gmail.com>
2026-04-08 07:40:59 +0530

    Add Loops/check_for_prime_no.java

---
+++ Loops/check_for_prime_no.java
@@ -0,0 +1,29 @@
+package Loops;
+
+import java.util.*;
+
+public class check_for_prime_no {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        sc.close();
+
+         if (n==2) {
+            System.out.println("n is Prime");
+         }
+         else {
+            boolean isPrime = true;
+            for(int i=2;i<=Math.sqrt(n);i++ )  {
+                if (n%i ==0 ) {
+                    isPrime = false;
+                    }
+            }
+            if ( isPrime == true) {
+                System.out.println("given no is Prime");
+            }
+            else {
+                System.out.println("given no is not Prime");
+            }
+        }
+    }
+}
