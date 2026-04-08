d9cbaa0253e8f8950f5d240e50feec2a64b1acbc
Aniket Chauhan <aniketchauhan4163@gmail.com>
2026-04-08 07:41:00 +0530

    Add Loops/loops_beginning.java

---
+++ Loops/loops_beginning.java
@@ -0,0 +1,40 @@
+package Loops;
+
+public class loops_beginning {
+ public static void main(String[] args) {
+        // int counter = 0;
+        // while (counter < 5) {
+        //     System.out.println("HELLO HELLO");
+        //     counter++;
+        // }
+        //     System.out.println("100x completed");
+
+        //-------------------------------------------------
+        //print 1 to n numbers
+        // int counter = 1;
+        // while (counter<=10) {
+        //     System.out.println(counter);
+        //     counter++;
+        // }
+        //-------------------------------------------------
+        //print 1 to n numbers
+    //     System.out.println("enter till number you want");
+    //     Scanner sc = new Scanner(System.in);
+    //     int range = sc.nextInt();
+        
+
+    
+    //     int counter = 1;
+    //     while (counter<= range) {
+    //         System.out.println(counter);
+    //     counter++;
+    // }
+
+    // sc.close();
+    //-------------------------------------------------
+        //
+        
+    }
+    
+}
+
