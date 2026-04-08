0874e7738640ca304c2cbfdd638dc076ae983142
Aniket Chauhan <aniketchauhan4163@gmail.com>
2026-04-08 07:41:01 +0530

    Add Patterns/inverted_star_pattern_02.java

---
+++ Patterns/inverted_star_pattern_02.java
@@ -0,0 +1,19 @@
+package Patterns;
+
+
+
+
+public class inverted_star_pattern_02 {
+    public static void main(String[] args) {
+        for(int i = 1;i<=4;i++) {
+            for(int j = 5-i; j>0;j--)
+            {System.out.print("*");
+        }
+            System.out.println("");
+        }
+
+
+    }
+    
+}
+
