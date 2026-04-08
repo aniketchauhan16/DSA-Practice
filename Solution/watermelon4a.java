a755f9b840b2e14fd9d0b745170c3f4fcc5eefb0
Aniket Chauhan <aniketchauhan4163@gmail.com>
2026-04-08 07:41:05 +0530

    Add Solution/watermelon4a.java

---
+++ Solution/watermelon4a.java
@@ -0,0 +1,21 @@
+package Solution;
+import java.util.*;
+
+public class watermelon4a {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        sc.close();
+        if(n==2){
+            System.out.println("NO");
+        }
+        else{
+            if(n%2==0) {
+                System.out.println("YES");
+            }
+            else {
+                System.out.println("NO");
+            }
+        }
+    }
+}
